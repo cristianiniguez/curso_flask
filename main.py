2b78482c118b7cdb931c3539e2b5a2b19bc7a468
cristianiniguez <felizpc1@gmail.com>
2021-08-15 21:15:29 -0400

    proyecto: app factory

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask.helpers import url_for
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
 import unittest
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from app import create_app
+from app.forms import LoginForm
 
-app.config['SECRET_KEY'] = 'SUPER SECRET'
+app = create_app()
 
 todos = ['Comprar café', 'Enviar una solicitud de compra', 'Entregar video a productor']
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Contraseña', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 
 
 @app.cli.command()
